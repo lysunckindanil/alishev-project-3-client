b8570297089638a1668db955ceec3f3c67de50c4
danila <lysunckin.danil@yandex.ru>
2024-01-24 23:11:31 +0300

    second commit

--- src/main/java/rest/project/sensor/models/Measurement.java
+++ src/main/java/rest/project/sensor/models/Measurement.java
@@ -2,14 +2,16 @@ package rest.project.sensor.models;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Measurement {
 
     private Integer value;
 
-    private boolean raining;
+    private Boolean raining;
 
     private Sensor sensor;
 }
--- src/main/java/rest/project/sensor/models/Sensor.java
+++ src/main/java/rest/project/sensor/models/Sensor.java
@@ -2,9 +2,11 @@ package rest.project.sensor.models;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Sensor {
     private String name;
 }
